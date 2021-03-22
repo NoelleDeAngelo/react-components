d31ea9482c193e1d93f9614ecdc133565df4fd16
Noelle DeAngelo <NoelleDeAngelo@gmail.com>
2021-03-22 11:25:55 -0700

    Add list items that turn bold when hovered over

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// TODO
+var GroceryList = (props)=> (
+  <ul>
+    {props.items.map(item => <GroceryListItem item = {item}/>)}
+  </ul>
+
+);
+
+
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      bold: false
+    }
+  }
+
+  onListItemHover() {
+    this.setState({
+      bold: !this.state.bold
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    }
+    return <li style = {style} onMouseOver={this.onListItemHover.bind(this)} onMouseOut = {this.onListItemHover.bind(this)}>{this.props.item}</li>
+  }
+}
+
+ReactDOM.render(<GroceryList items = {['Corn', 'Jam']}/>, document.getElementById("app"));
